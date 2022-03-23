3ec90c4644c7eda54762cb9e65fd21cee90cbc26
natnaelawel <natnael.awel@gmail.com>
2022-03-24 02:34:40 +0300

    animations and some style update

--- src/components/Dapp/DappMain/NodesData/NodesData.component.tsx
+++ src/components/Dapp/DappMain/NodesData/NodesData.component.tsx
@@ -9,8 +9,8 @@ const NodesSection = (props: Props) => {
     <div className="w-5/6">
       <div className="toolbar_gradient p-0.5 rounded-2xl my-10">
         <div className=" bg-slate-900 rounded-2xl min-w-full min-h-full flex flex-row sm:flex-col items-start lg:items-center justify-start sm:divide-y divide-slate-600">
-          <div className=" md:w-full flex flex-col justify-start items-start md:items-center py-5">
-            <div className="w-full flex flex-col sm:flex-row sm:items-center justify-between">
+          <div className=" md:w-full max-w-full overflow-x-auto flex flex-col justify-start items-start md:items-center py-5">
+            <div className="w-full flex flex-col sm:flex-row sm:items-center justify-between gap-x-4">
               <div className="sm:w-1/6 p-4 text-white">
                 <p className="font-bold text-xl">MyNodes</p>
               </div>
@@ -28,7 +28,7 @@ const NodesSection = (props: Props) => {
                 <p className="font-bold text-xl">Rewards</p>
               </div>
               <div className="sm:w-1/6 flex flex-col gap-3 items-start justify-center md:justify-center md:items-center">
-                <button className="min-w-[8em] md:min-w-[10em] md:border-2 border-slate-500 bg-transparent text-white px-2 sm:px-8 sm:py-3 rounded-lg">
+                <button className="min-w-[6em] lg:min-w-[8em] xl:min-w-[10em] md:border-2 border-slate-500 bg-transparent text-white px-2 sm:px-8 sm:py-3 rounded-lg">
                   Claim All
                 </button>
               </div>
@@ -73,18 +73,26 @@ const NodesSection = (props: Props) => {
               <div className="min-w-[50em] min-h-[20em] md:min-h-auto h-full w-full flex md:flex-col items-end justify-end sm:items-center md:justify-center gap-0 md:gap-3 ">
                 {[1, 2, 3, 4].map((index) => {
                   return (
-                    <div className="py-2 border-l-2 md:border-none border-slate-600  min-h-[22em] md:min-h-[4em]  justify-end w-full h-full flex flex-col md:flex-row">
-                      <div className="w-full h-full sm:w-5/6 flex flex-col sm:flex-row sm:items-center justify-center">
-                        <div className="w-24 flex flex-col justify-center ">
-                          <p className="py-5 uppercase text-center text-slate-500 text-xl font-bold"></p>
-                        </div>
+                    <div className="w-full py-2 border-l-2 md:border-none text-gray-300 border-slate-600  min-h-[22em] md:min-h-[4em]  justify-end h-full flex flex-col md:flex-row">
+                      <div className="sm:w-1/6 p-4 ">
+                        <p className=" text-xl">MyNodes</p>
                       </div>
-                      <div className="sm:w-1/6 flex flex-col gap-3 justify-center items-center">
-                        <div className="">
-                          <button className="min-w-[10em] md:border-2 border-slate-500 bg-transparent text-slate-500 px-2 sm:px-8 sm:py-3 rounded-lg">
-                            Claim
-                          </button>
-                        </div>
+                      <div className="sm:w-1/6 p-4 ">
+                        <p className=" text-xl">Type</p>
+                      </div>
+                      <div className="sm:w-1/6 p-4 flex items-center ">
+                        <p className="mx-3  text-xl">RPC</p>
+                      </div>
+                      <div className="sm:w-1/6 p-4 ">
+                        <p className=" text-xl">Due Date</p>
+                      </div>{" "}
+                      <div className="sm:w-1/6 p-4 ">
+                        <p className=" text-xl">Rewards</p>
+                      </div>
+                      <div className="sm:w-1/6 flex flex-col gap-3 items-start justify-center md:justify-center md:items-center">
+                        <button className="min-w-[8em] md:min-w-[10em] md:border-2 border-slate-500 bg-transparent text-gray-500 px-2 sm:px-8 sm:py-3 rounded-lg">
+                          Claim
+                        </button>
                       </div>
                     </div>
                   );
